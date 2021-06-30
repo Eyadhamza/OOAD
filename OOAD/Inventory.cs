6a75a756b363b98f5208cc36a8bcf62ae4c8ad38
EyadHamza <EyadHamza0@outlook.com>
2021-06-30 09:22:50 +0200

    Figuring out enums in c#

--- OOAD/Inventory.cs
+++ OOAD/Inventory.cs
@@ -1,7 +1,36 @@
-﻿namespace OOAD
+﻿using System.Collections.Generic;
+
+namespace OOAD
 {
     public class Inventory
     {
-        
+        private LinkedList<Guitar> _guitars;
+
+        public LinkedList<Guitar> Guitars
+        {
+            get => _guitars;
+            set => _guitars = value;
+        }
+
+        public Inventory(LinkedList<Guitar> guitars)
+        {
+            _guitars = guitars;
+        }
+
+        public void AddGuitar()
+        {
+
+
+        }
+
+        public Guitar GetGuitar()
+        {
+            return null;
+        }
+
+        public List<Guitar> Search()
+        {
+            return null;
+        }
     }
 }
--- OOAD/Wood.cs
+++ OOAD/Wood.cs
@@ -2,6 +2,27 @@
 {
     public enum Wood
     {
-        
+        INDIAN_ROSEWOOD, BRAZILIAN_ROSEWOOD, MAHOGANY,
+    MAPLE, COCOBOLO, CEDAR, ADIRONDACK, ALDER, SITKA
+
+
+
     }
+
+static class WoodMethods
+{
+    public static string toString(this Wood wood)
+    {
+        switch (wood)
+        {
+            case Wood.INDIAN_ROSEWOOD:
+                return "Indian Rosewood";
+            case Wood.BRAZILIAN_ROSEWOOD:
+                return "Brazilian Rosewood";
+            default:
+                return "What?!";
+        }
+    }
+}
+
 }

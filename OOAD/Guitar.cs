ed4db7f2659d6250e14b6a5d0de36516176981d8
EyadHamza <EyadHamza0@outlook.com>
2021-06-30 09:06:02 +0200

    wip

--- OOAD/Guitar.cs
+++ OOAD/Guitar.cs
@@ -2,6 +2,35 @@
 {
     public class Guitar
     {
-        
+        private string _serialNumber;
+        private double _price;
+        private GuitarSpec _spec;
+
+        public string SerialNumber
+        {
+            get => _serialNumber;
+            set => _serialNumber = value;
+        }
+
+        public double Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public GuitarSpec Spec
+        {
+            get => _spec;
+            set => _spec = value;
+        }
+
+        public Guitar(GuitarSpec spec, double price, string serialNumber)
+        {
+            this._spec = spec;
+            this._price = price;
+            this._serialNumber = serialNumber;
+        }
+
+
     }
 }
